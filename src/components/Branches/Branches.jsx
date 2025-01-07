6ce3dc968f66000ccdbd000839449cff1d0bd1a7
AminaAbdElAzeez <aminaabdelazeez1999@gmail.com>
2025-01-07 18:45:33 +0200

    changes

--- src/components/Branches/Branches.jsx
+++ src/components/Branches/Branches.jsx
@@ -16,21 +16,43 @@ function Branches() {
       desc: t("branch1Desc"),
       address: t("branch1Address"),
       phone: "16226",
+      lat: 30.02175,
+      long: 31.4443611,
     },
     {
       desc: t("branch2Desc"),
       address: t("branch2Address"),
       phone: "16226",
+      lat: 30.588146,
+      long: 31.4877952,
     },
     {
       desc: t("branch3Desc"),
       address: t("branch3Address"),
       phone: "16226",
+      lat: 30.9779279,
+      long: 31.1713644,
     },
     {
       desc: t("branch4Desc"),
       address: t("branch4Address"),
       phone: "16226",
+      lat: 30.788313,
+      long: 31.000022,
+    },
+    {
+      desc: t("branch5Desc"),
+      address: t("branch5Address"),
+      phone: "16226",
+      lat: 30.799004,
+      long: 31.00132,
+    },
+    {
+      desc: t("branch6Desc"),
+      address: t("branch6Address"),
+      phone: "16226",
+      lat: 30.7959398,
+      long: 30.9919414,
     },
   ];
 
@@ -82,11 +104,10 @@ function Branches() {
                   <p className="branch-desc">{branch.desc}</p>
                   <a
                     className="branch-address"
-                    href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-                      branch.address
-                    )}`}
+                    href={`https://www.google.com/maps?q=${branch.lat},${branch.long}`}
                     target="_blank"
                     rel="noopener noreferrer"
+                    style={{ color: "#1890ff", cursor: "pointer" }}
                   >
                     {branch.address}
                   </a>
